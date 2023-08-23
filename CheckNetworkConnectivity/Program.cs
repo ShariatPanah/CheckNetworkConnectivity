01eeb3ce1407e3ec8d857811bb398b52718b06af
Sirwan ShariatPanah <er.sirwan@live.com>
2023-08-23 13:12:12 +0330

    Add project files.

---
+++ CheckNetworkConnectivity/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Net.NetworkInformation;
+
+namespace CheckNetworkConnectivity
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NetworkChange.NetworkAvailabilityChanged += NetworkAvailabilityChanged;
+
+            Console.ReadKey();
+        }
+
+        private static void NetworkAvailabilityChanged(object sender, NetworkAvailabilityEventArgs e)
+        {
+            if (e.IsAvailable)
+            {
+                Console.WriteLine("Available.");
+            }
+            else
+            {
+                Console.WriteLine("Not available.");
+            }
+        }
+    }
+}
